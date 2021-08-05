da86e47e5a707c334c9a5cd4c5332952f1b6b7de
liangzhi <2490299601@qq.com>
2021-08-05 17:38:43 +0800

    更新代码

--- cloud-provider-payment8001/src/main/java/com/lz/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/lz/springcloud/PaymentMain8001.java
@@ -3,9 +3,11 @@ package com.lz.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
 
     public static void main(String[] args) {
